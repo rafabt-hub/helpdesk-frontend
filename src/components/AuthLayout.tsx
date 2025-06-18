936b714484295cdf51ae108e8c453b9935dacfd8
Rafael <rafaelbthomaz@gmail.com>
2025-06-17 22:28:33 -0300

    final ajustments

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -7,8 +7,8 @@ export function AuthLayout() {
   return (
     <div className="w-screen h-screen bg-cover bg-center flex overflow-hidden" style={{ backgroundImage: `url(${bgImage})` }}>
       <div className="hidden lg:flex flex-1" />
-      <main className="w-full lg:w-1/2 mt-10 h-full bg-white flex flex-col px-8 py-10 items-center lg:px-20 lg:py-16 rounded-t-[2rem] lg:rounded-tr-none lg:rounded-bl-[2rem] shadow-2xl">
-        <div className="flex items-center justify-center gap-4 mb-10 lg:gap-8 lg:mt-20">
+      <main className="w-full lg:w-1/2 mt-10 h-full bg-white flex flex-col items-center p-4 rounded-t-[2rem] lg:rounded-tr-none lg:rounded-bl-[2rem] shadow-2xl">
+        <div className="flex items-center justify-center gap-4 mb-5 lg:gap-8 lg:mt-20 lg:mb-20">
           <img src={logoSvg} alt="Logo" className="w-14 h-14 lg:w-48 lg:h-48" />
           <h1 className="text-3xl lg:text-8xl text-[var(--color-blue-100)] font-bold">HelpDesk</h1>
         </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ type Props = React.ComponentProps<"input"> & {
 
 export function Input({ legend, ...rest }: Props) {
   return (
-    <fieldset className="flex flex-col gap-2 focus-within:text-[var(--color-blue-100)]">
+    <fieldset className="w-full flex flex-col gap-2 focus-within:text-[var(--color-blue-100)]">
       {legend && (<legend className="lg:text-6xl text-inherit">{legend}</legend>)}
 
       <input className="border-b border-[var(--color-gray-400)] focus:border-[var(--color-blue-100)] focus:outline-none py-2 px-1 lg:mt-5 lg:py-6 lg:px-10 lg:text-6xl" {...rest} />

6cb56e5c1f6cc2d686abab05253daf3ee342950e
Rafael <rafaelbthomaz@gmail.com>
2025-07-15 16:31:04 -0300

    navigation fix

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -9,7 +9,7 @@ export function SideBar() {
     <nav className="flex-1">
           <ul className="space-y-2">
             <li>
-              <Link to="tickets" className="flex items-center p-3 rounded-lg text-gray-400 hover:bg-[var   (--color-blue-100)] hover:text-white">
+              <Link to="/" className="flex items-center p-3 rounded-lg text-gray-400 hover:bg-[var   (--color-blue-100)] hover:text-white">
                <img className="w-5 h-5 mr-4" src={clipboard} alt="lista" />
                <span className="font-medium">Chamados</span>
               </Link>
